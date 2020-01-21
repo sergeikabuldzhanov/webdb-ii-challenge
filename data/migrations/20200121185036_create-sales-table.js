779e3445d62af65c83904c0664f7efbee9c60f12
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-21 21:02:38 +0500

    Fixed sales migrations

--- data/migrations/20200121185036_create-sales-table.js
+++ data/migrations/20200121185036_create-sales-table.js
@@ -1,5 +1,6 @@
 exports.up = function(knex) {
   return knex.schema
+    .dropTableIfExists("cars")
     .createTable("sales", table => {
       table.increments("saleid");
       table.integer("price").notNullable();
@@ -8,19 +9,53 @@ exports.up = function(knex) {
         .integer("carid")
         .unsigned()
         .references("id")
-        .inTable("cars");
+        .inTable("cars")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
     })
-    .table("cars", table => {
+    .createTable("cars", table => {
+      table.increments();
+      //unique id auto-increment
+      table.text("VIN").notNullable();
+      //"vin", text, required
+      table.text("make").notNullable();
+      //"make", text, required
+      table.text("model").notNullable();
+      //"model", text, required
+      table.decimal("mileage").notNullable();
+      //"mileage", float, required
+      table.text("transmission");
+      //"transmission", text, not required
+      table.text("status");
+      //"status", text, not required
       table
         .integer("saleid")
         .unsigned()
         .references("saleid")
-        .inTable("sales");
+        .inTable("sales")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
     });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIdExists("sales").table("cars", table => {
-    table.dropColumn("saleid");
-  });
+  return knex.schema
+    .dropTableIfExists("sales")
+    .dropTableIfExists("cars")
+    .createTable("cars", table => {
+      table.increments();
+      //unique id auto-increment
+      table.text("VIN").notNullable();
+      //"vin", text, required
+      table.text("make").notNullable();
+      //"make", text, required
+      table.text("model").notNullable();
+      //"model", text, required
+      table.decimal("mileage").notNullable();
+      //"mileage", float, required
+      table.text("transmission");
+      //"transmission", text, not required
+      table.text("status");
+      //"status", text, not required
+    });
 };
